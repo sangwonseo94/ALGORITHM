456e5c4c9f5dba0a7953faadb8af82e9111e5402
서상원 <gotkddnjs@naver.com>
2020-03-22 23:27:40 +0900

    2020-03-22

---
+++ 배열의분할.java
@@ -0,0 +1,39 @@
+package 풀문제;
+
+import java.util.Scanner;
+
+public class 배열의분할 {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1 ; tc<= T ; tc++) {
+			int n = sc.nextInt();
+			int ans =1;
+			int now = 0;
+			int pre1 = 0 ;
+			int pre2 = 0 ;
+			for(int i = 0; i < n ; i ++) {
+				now = sc.nextInt();
+				if(pre2 ==0) {
+					pre2 = now;
+				}
+				else if(pre2 != 0 && pre1 == 0) {
+					pre1 = now;
+				}
+				else if(pre1 == now) continue;
+				else if( (pre2 < pre1 && pre1 > now) || (pre2 > pre1 && pre1 < now)) {
+					ans++;
+					pre2 = now;
+					pre1 = 0;
+				}
+				else {
+					
+				}
+			}
+			System.out.println("#"+tc+" "+ans);
+		}
+		
+	}
+
+}
---
+++ 서로소집합.java
@@ -0,0 +1,40 @@
+package 풀문제;
+
+import java.util.Scanner;
+
+public class 서로소집합 {
+	static int parent[];
+	public static int find(int a) {
+		if(parent[a] == a) return a;
+		return find(parent[a]);
+	}
+	public static void union(int a , int b) {
+		// 부모끼리 합친다.
+		int pa = find(a);
+		int pb = find(b);
+		if(pa==pb) return;
+		if(pa>pb) { parent[pb] = pa;}
+		else { parent[pa] = pb;}
+	}
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		parent = new int[n+1];
+		for(int i = 0 ; i <= n ; i ++) parent[i] =i;
+		for(int i = 0 ; i < m ; i ++) {
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a == 0) {
+			union(b,c);
+		}else {
+			if(find(b) == find(c)) {
+				System.out.print("YES\n");
+			}else {
+				System.out.print("NO\n");
+			}
+		}
+	}
+	}
+}
