98206e508d7bb9f375ae8d27d6d594e7f7a21c09
SangwonSeo <gotkddnjs@naver.com>
2020-09-14 19:05:49 +0900

    202-09-14

--- Baekjoon Online Judge/이차원배열과연산.java
+++ Baekjoon Online Judge/이차원배열과연산.java
@@ -1,137 +1,73 @@
-package 풀문제;
+package algorithm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class 이차원배열과연산 {
-	static class node{
-		int y,x,dir;
-		ArrayList<Integer> up = new ArrayList<>();
-		public node(int num,int y,int x,int dir) {
-			up.add(num);
-			this.y = y;
-			this.x = x;
-			this.dir = dir;
+	
+	public static PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			if(o1[1]>o2[1]) return 1;
+			else if(o1[1]<o2[1]) return -1;
+			else {return o1[0]-o2[0];}
 		}
-	}
+	});
 	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken())-1;
+		int c = Integer.parseInt(st.nextToken())-1;
 		int k = Integer.parseInt(st.nextToken());
-		int map[][] = new int[n+1][n+1];
-		for(int y= 1 ; y <=n ; y++) {
-			st = new StringTokenizer(br.readLine());
-			for(int x =1 ; x<= n ; x++) {
-				map[y][x] = Integer.parseInt(st.nextToken());
+		int map[][] = new int[3][3];
+		for(int i=0;i<3;i++) {st=new StringTokenizer(br.readLine());for(int j=0;j<3;j++)map[i][j]=Integer.parseInt(st.nextToken());}
+		int answer = -1;
+		for(int i=0;i<=100;i++){
+			int y=map.length;int x=map[0].length;
+			if(x>100) {for(int yyy=0;yyy<y;yyy++) map[yyy]=Arrays.copyOfRange(map[yyy], 0, 100);}
+			else if(y>100){
+				int cmap[][] =new int[100][x];
+				for(int yyy=0;yyy<100;yyy++)cmap[yyy]=map[yyy].clone();
+				map=cmap;
 			}
-		}
-		int dir[][] = {{0,0},{0,1},{0,-1},{-1,0},{1,0}};
-		node node_list[] = new node[k+1];
-		for(int index = 0 ; index < k ; index++) {
-			st = new StringTokenizer(br.readLine());
-			node_list[index+1] = new node(index+1, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		}
+			if(r<y && c<x && map[r][c]==k) {answer=i;break;}
+			if(y>=x) { // r연산
+				int maxsize=-1; 
+				for(int yy=0;yy<y;yy++) {map[yy] = compute(map[yy]);maxsize = maxsize > map[yy].length ? maxsize : map[yy].length;}
+				for(int yy=0;yy<y;yy++) {int row[] = new int[maxsize];for(int xx=0;xx<maxsize;xx++) {if(xx<map[yy].length) row[xx]=map[yy][xx];}map[yy] =row;}
+			}else {
+				int tempmap[][]=new int[x][y]; int maxsize =-1;
+				for(int xx=0;xx<x;xx++) {int col[]= new int[y]; for(int yy=0;yy<y;yy++) {col[yy] = map[yy][xx];}col = compute(col);maxsize = maxsize > col.length ? maxsize : col.length;tempmap[xx] = col;}
+				map = new int[maxsize][x];
+				for(int xx=0;xx<x;xx++) {
+					int col[] = new int[maxsize];
+					for(int yy=0;yy<tempmap[xx].length;yy++) {col[yy] = tempmap[xx][yy];}
+					for(int yy=0;yy<maxsize;yy++) {	map[yy][xx] = col[yy];}
+				}
+			}
+		}System.out.println(answer);
 		
-		int time =1;
-		top :
-		while(time <=1000) {
-			for(int index=1; index<=k ; index++) {
-				
-				
-				int nx = node_list[index].x+dir[node_list[index].dir][1];
-				int ny = node_list[index].y+dir[node_list[index].dir][0];
-				
-				if(nx >= 1 && ny >=1 && ny <= n && nx <= n && map[ny][nx] !=2) {
-					if(map[ny][nx]==0) {
-						//흰색 = 좌표가 곂치는 것들중에서 현재 index의 up요소들을 전부 삭제해준다.
-						for(int i = 1 ; i <= k ; i++) {
-							if(i==index)continue;
-							
-							if(node_list[i].y == node_list[index].y && node_list[i].x == node_list[index].x) {
-								// 좌표가 같은경우 index의 up요소를 가진 것들을 모두 삭제해줌.
-								for(int a = 0 ; a < node_list[index].up.size();a++) {
-									for(int b = 0 ; b < node_list[i].up.size(); b++) {
-										if(node_list[index].up.get(a) == node_list[i].up.get(b)) {
-											node_list[i].up.remove(b); b--;
-										}
-									}
-								}
-							}
-						}
-						
-						for(int i = 1 ; i <= k ; i++) {
-							if(i==index)continue;
-							
-							if(node_list[i].y == ny && node_list[i].x == nx) {
-								for(int j = 0 ; j < node_list[index].up.size();j++) {
-									node_list[i].up.add(node_list[index].up.get(j));
-								}
-							}
-						}
-						node_list[index].x = nx ;
-						node_list[index].y = ny ;
-					}
-					else if(map[ny][nx]== 1) {
-						
-						// 빨강
-						for(int i = 1 ; i <= k ; i++) {
-							if(i==index)continue;
-							if(node_list[i].y == node_list[index].y && node_list[i].x == node_list[index].x) {
-								// 좌표가 같은경우 index의 up요소를 가진 것들을 모두 삭제해줌.
-								for(int a = 0 ; a < node_list[index].up.size();a++) {
-									for(int b = 0 ; b < node_list[i].up.size(); b++) {
-										if(node_list[index].up.get(a) == node_list[i].up.get(b)) {
-											node_list[i].up.remove(b); b--;
-										}
-									}
-								}
-							}
-						}
-						
-						
-						ArrayList<Integer> temp = new ArrayList<>();
-						for(int a = node_list[index].up.size()-1 ; a >=0 ; a--) {
-							temp.add(node_list[index].up.get(a));
-						}
-						node_list[index].up.clear();
-						for(int a = 0 ; a < temp.size(); a++) {
-							node_list[index].up.add(temp.get(a));
-						}
-						temp.clear();
-						for(int i = 1 ; i <= k ; i++) {
-							if(i==index)continue;
-							if(node_list[i].y == ny && node_list[i].x == nx) {
-								for(int j = 0 ; j < node_list[index].up.size();j++) {
-									node_list[i].up.add(node_list[index].up.get(j));
-								}
-							}
-						}
-						node_list[index].x = nx ;
-						node_list[index].y = ny ;
-					}
-				}
-				else {
-					int ndir = node_list[index].dir;
-					
-					if(ndir ==1) ndir = 2;
-					else if(ndir==2) ndir =1;
-					else if(ndir==3) ndir =4;
-					else if(ndir==4) ndir =3;
-					node_list[index].dir = ndir;
-					
-					
-				}
-			}
-			
-			time++;
-		}
-		if(time == 1001) System.out.println(-1);
-		else System.out.println(time);
 		
 	}
-
+	private static int[] compute(int[] is) {
+		if(is.length >100) is= Arrays.copyOfRange(is, 0, 100);
+		int number=0;int show=0;
+		Arrays.sort(is);
+		for(int i=0;i<is.length;i++) {
+			if(is[i]==0) continue;
+			if(number==0&&show==0){number=is[i];show=1;}
+			else if(number==is[i]&&show!=0)show++;
+			else {pq.add(new int[] {number,show}); number=is[i];show=1;}
+		}
+		pq.add(new int[] {number,show});
+		int res[] = new int[pq.size()*2]; int index=0;
+		while(!pq.isEmpty()) {int[]now =pq.poll();res[index++]=now[0];res[index++]=now[1];}
+		return res;
+	}
 }
+
