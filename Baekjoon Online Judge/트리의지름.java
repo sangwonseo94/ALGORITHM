57710ddc3ee637fa1dc69a241b0a22caf63c140c
SangwonSeo <gotkddnjs@naver.com>
2020-10-03 00:31:14 +0900

    2020-10-03

--- Baekjoon Online Judge/트리의지름.java
+++ Baekjoon Online Judge/트리의지름.java
@@ -1,32 +1,81 @@
-package algorithm;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class 트리의지름 {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		/*
-		 * 12
-			1 2 3
-			1 3 2
-			2 4 5
-			3 5 11
-			3 6 9
-			4 7 1
-			4 8 7
-			5 9 15
-			5 10 4
-			6 11 6
-			6 12 10
-		 */
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		ArrayList<int []> node[] = new ArrayList[n];
+		for(int i=0;i<n;i++) node[i] = new ArrayList<>();
+		for(int i=0;i<n-1;i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken())-1;
+			int e = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken());
+			node[s].add(new int[] {e,c});
+			node[e].add(new int[] {s,c});
+		}
 		
+		int s = 0;
+		int cost[] = new int[n];
+		boolean visit[] = new boolean[n];
 		
-		
-		
-	}
+		cost[s] = 0; visit[s] = true;
+		ArrayDeque<int []> dq = new ArrayDeque<>();
+		dq.add(new int[] {s,0});
+		while(!dq.isEmpty()) {
+			int [] now = dq.poll();
+			for(int i=0; i< node[now[0]].size() ; i++) {
+				int next[] = node[now[0]].get(i);
+				if(!visit[next[0]]) {
+					visit[next[0]] = true;
+					cost[next[0]] = now[1] + next[1];
+					dq.add(new int[] {next[0], now[1]+ next[1]});
+					
+				}
+			}
+		}
 
+		int max_idx = -1;
+		int max_val = -1;
+		
+		for(int i=0;i<n;i++) {
+			if(cost[i] > max_val) {
+				max_val = cost[i];
+				max_idx =i;
+			}
+		}
+		
+		cost = new int[n];
+		visit = new boolean[n];
+		
+		cost[max_idx] = 0; visit[max_idx] = true;
+		
+		dq.add(new int[] {max_idx,0});
+		while(!dq.isEmpty()) {
+			int [] now = dq.poll();
+			for(int i=0; i< node[now[0]].size() ; i++) {
+				int next[] = node[now[0]].get(i);
+				if(!visit[next[0]]) {
+					visit[next[0]] = true;
+					cost[next[0]] = now[1] + next[1];
+					dq.add(new int[] {next[0], now[1]+ next[1]});
+					
+				}
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			if(cost[i] > max_val) {
+				max_val = cost[i];
+				max_idx =i;
+			}
+		}
+		System.out.println(max_val);
+	}
 }
