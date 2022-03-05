040f6d94b819e75caf9150be72a7f4d56ca3a343
Sangwonseo <43750294+sangwonseo94@users.noreply.github.com>
2022-03-05 10:50:22 +0900

    Add files via upload

---
+++ Baekjoon Online Judge/BOJ_11047.java
@@ -0,0 +1,31 @@
+package algorithm_2022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11047 {
+
+	public static void main(String[] args) throws IOException {
+       BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int N = Integer.parseInt(st.nextToken());
+       int K = Integer.parseInt(st.nextToken());
+       int answer =0;
+       int Num[] = new int[N];
+       for(int i=0;i<N;i++) {
+    	   Num[i] = Integer.parseInt(br.readLine());
+       }
+       
+       while(K!=0) {
+    	    if(K-Num[N-1] >= 0) {
+    		       K-=Num[N-1];
+    		       answer++;
+            } else {
+    	    N--;	
+            } 
+       }
+       System.out.println(answer);              
+	}
+}
